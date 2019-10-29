d751ac5265e69023fca40e378888603216af6928
Kaplas80 <kaplas80@gmail.com>
2019-10-29 13:21:21 +0100

    Refactor Program.cs

--- ParTool/Program.cs
+++ ParTool/Program.cs
@@ -7,14 +7,12 @@
 namespace ParTool
 {
     using System;
-    using System.Collections.Generic;
-    using System.IO;
     using CommandLine;
 
     /// <summary>
     /// Main program.
     /// </summary>
-    internal static class Program
+    internal static partial class Program
     {
         private static void Main(string[] args)
         {
@@ -22,31 +20,11 @@ namespace ParTool
                 .WithParsed<ListOptions>(List);
         }
 
-        private static void List(ListOptions opts)
+        private static void WriteHeader()
         {
             Console.WriteLine(CommandLine.Text.HeadingInfo.Default);
             Console.WriteLine(CommandLine.Text.CopyrightInfo.Default);
             Console.WriteLine();
-
-            if (!File.Exists(opts.ParFile))
-            {
-                Console.WriteLine($"ERROR: \"{opts.ParFile}\" not found!!!!");
-                return;
-            }
-
-            IList<ParLib.Par.FileInfo> parContents = ParLib.Api.GetParContents(opts.ParFile);
-
-            foreach (ParLib.Par.FileInfo info in parContents)
-            {
-                Console.WriteLine($"{info.Path}\t{info.Size} bytes\t{info.FileDate:G}");
-            }
-        }
-
-        [Verb("list", HelpText = "Show contents from a Yakuza PAR file.")]
-        private class ListOptions
-        {
-            [Option('i', "input", Required = true, HelpText = "Yakuza PAR file")]
-            public string ParFile { get; set; }
         }
     }
 }
